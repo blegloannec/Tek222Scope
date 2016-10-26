9e10a52830d04802118972eef2c05eb313b318d4
blegloannec <blg@gmx.com>
2016-10-26 15:11:13 +0200

    cleaning

--- tek222.py
+++ tek222.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
-
 import serial
 
-# NB: Utiliser le channel *actif* sur l'oscillo pour recuperer les donnees en mode XY !!
+# NB: Utiliser le channel *actif* sur l'oscillo pour recuperer les donnees
+#     en mode XY !
 
 class Tek222:
-    def __init__(self,device):
+    def __init__(self, device):
+        # Noms des boutons (cf manuel)
         self.Buttons = {'CLEAR' : '1',
                         'Menu Item 0' : '2',
                         'Menu Item 1' : '3',
@@ -29,9 +29,13 @@ class Tek222:
                         'X10 Mag' : '20',
                         'Variable Gain' : '21',
                         'AUTO LVL: PUSH' : '22'}
+        # Ouverture de port serie (peut lancer une exception, non rattrapee ici)
         self.SerialPort = serial.Serial(device,9600,timeout=None,xonxoff=1)
- 
-    def simulate_button(self,button):
+
+    def close(self):
+        self.SerialPort.close()
+        
+    def simulate_button(self, button):
         self.SerialPort.write('BUT %s\r'%(self.Buttons[button]))
 
     def get_id(self):
@@ -50,11 +54,19 @@ class Tek222:
         self.SerialPort.write('CAL?\r')
         return self.SerialPort.readline()[:-1]
 
-    def print_curve(self,channel):
+    def get_curve(self, channel):
+        self.SerialPort.write('CURV? %s\r'%(channel))
+        res = self.SerialPort.readline()
+        ps = 6+len(channel)
+        byte_count = res[ps+12:ps+16]
+        wf_size = int(byte_count,16)
+        waveform = res[ps+16:ps+16+2*wf_size]
+        return waveform
+
+    def print_curve(self, channel):
         # channel in {CH1, CH2, REF1, ..., REF4}
         self.SerialPort.write('CURV? %s\r'%(channel))
-        res = self.SerialPort.readline()#[6+len(channel):-2]
-        #print res
+        res = self.SerialPort.readline()
         ps = 6+len(channel)
         resp_pref = res[:ps]
         print resp_pref
@@ -69,15 +81,3 @@ class Tek222:
         print waveform
         checksum = res[ps+16+2*wf_size:ps+18+2*wf_size]
         print checksum
-
-    def get_curve(self,channel):
-        self.SerialPort.write('CURV? %s\r'%(channel))
-        res = self.SerialPort.readline()
-        ps = 6+len(channel)
-        byte_count = res[ps+12:ps+16]
-        wf_size = int(byte_count,16)
-        waveform = res[ps+16:ps+16+2*wf_size]
-        return waveform
-
-    def close():
-        self.SerialPort.close()
